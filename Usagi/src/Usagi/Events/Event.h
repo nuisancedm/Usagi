8b1c0a2fc3fbf7e0d57a631e68bb7203ad0d5daa
nuisancedm <2019684871@qq.com>
2024-04-14 17:58:53 +0800

    add cmakelists

--- Usagi/src/Usagi/Events/Event.h
+++ Usagi/src/Usagi/Events/Event.h
@@ -33,7 +33,7 @@ namespace Usagi {
 		EventCategoryMouseButton = BIT(4), // 10000
 	};
 
-#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; } \
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::type; } \
 								virtual EventType GetEventType() const override { return  GetStaticType(); } \
 								virtual const char* GetName() const override {return #type; }
 
